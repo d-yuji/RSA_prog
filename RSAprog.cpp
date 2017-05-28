34bf81be66a7c2916ae5b21789177c1626e725db
YujiDouken <d.yuji.fm@gmail.com>
2017-05-28 15:58:53 +0900

    define gcd and lcm

--- RSAprog.cpp
+++ RSAprog.cpp
@@ -4,8 +4,18 @@
 
 using namespace std;
 
+int gcd(int p,int q);//最大公約数
+int lcm(int p,int q);//最小公倍数
+
 int main(int argc, char const *argv[])
 {
 	printf("Hello World\n");
 	return 0;
+}
+
+int gcd(int p,int q){
+
+}
+int lcm(int p,int q){
+
 }
