246c4197d48be0b64046fe5662c3e741d7bc5022
YujiDouken <d.yuji.fm@gmail.com>
2017-05-28 16:10:05 +0900

    implement gcd and swap

--- RSAprog.cpp
+++ RSAprog.cpp
@@ -6,16 +6,36 @@ using namespace std;
 
 int gcd(int p,int q);//最大公約数
 int lcm(int p,int q);//最小公倍数
+void swap(int *a,int *b);//入れ替え
 
 int main(int argc, char const *argv[])
 {
-	printf("Hello World\n");
+	printf("%d\n",gcd(12,24));
 	return 0;
 }
 
 int gcd(int p,int q){
+	int temp;
+	int a = p;
+	int b = q;
 
+	if(a<b){swap(&a,&b);}
+
+	while(temp!=0){
+		a = b;
+		b = temp;
+		temp = a % b;
+	}
+	return b;
 }
+
 int lcm(int p,int q){
 
+}
+void swap(int *a,int *b){
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+	return;
 }
