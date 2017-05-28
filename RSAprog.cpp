61ca0bf0fee60ab66a7e929ef24c6ebf5014bf9d
YujiDouken <d.yuji.fm@gmail.com>
2017-05-28 17:21:39 +0900

    make d e

--- RSAprog.cpp
+++ RSAprog.cpp
@@ -1,17 +1,27 @@
 #include <iostream>
 #include <cstdio>
 #include <cmath>
+#include <string>
 
 using namespace std;
 
 int gcd(int p,int q);//最大公約数
 int lcm(int p,int q);//最小公倍数
 void swap(int *a,int *b);//入れ替え
+int makeE(int l);
+int makeD(int l,int e);
 
 int main(int argc, char const *argv[])
 {
-	printf("%d\n",gcd(15,17));
-	printf("%d\n",lcm(15,17));
+	string str("Hello_world");
+	int p = 227;
+	int q = 4099;
+	int n = p*q;
+	int lambda = lcm(p-1,q-1);
+	int e = makeE(lambda);
+	int d = makeD(lambda,e);
+
+	printf("%d %d %d %d\n",n,lambda,e,d);
 	return 0;
 }
 
@@ -39,4 +49,25 @@ void swap(int *a,int *b){
 	*a = *b;
 	*b = temp;
 	return;
+}
+
+int makeE(int l){
+	int e = 2;
+	while(1){
+		if(e==1||gcd(l,e)==1){
+			break;
+		}
+		e++;
+	}
+	return e;
+}
+int makeD(int l,int e){
+	int d = 2;
+	while(1){
+		if(d==l||(e*d)%l == 1){
+			break;
+		}
+		d++;
+	}
+	return d;
 }
