dc5dce5aafdf257d84a52040db6d5b157e5a437d
YujiDouken <d.yuji.fm@gmail.com>
2017-06-07 17:27:52 +0900

    implement encryption function

--- RSAprog.cpp
+++ RSAprog.cpp
@@ -11,12 +11,24 @@ void swap(int *a,int *b);//入れ替え
 int makeE(int l);
 int makeD(int l,int e);
 
+void encryption();
+void decryption();
+
+int Pow2[33];
+
+
 int main(int argc, char const *argv[])
 {
 	string str("Hello_world");
 	int p = 227;
 	int q = 4099;
 	int n = p*q;
+
+	Pow2[0] = 1;
+	for (int i = 1; i < 31; ++i){
+		Pow2[i] = Pow2[i-1]*2;
+	}
+
 	int lambda = lcm(p-1,q-1);
 	int e = makeE(lambda);
 	int d = makeD(lambda,e);
@@ -71,3 +83,45 @@ int makeD(int l,int e){
 	}
 	return d;
 }
+void encryption(string message,int e,int n,string result){
+/*
+	message 暗号化する文字列
+	e 文字列を累乗する値
+	n 文字列をmodする値
+	result 暗号化された文字列
+	PowM 文字の値を2のべき乗した値からmod nした値の配列
+	enc 暗号化された値
+	indexPow2 2のべき乗のインデックス
+*/
+	int PowM[33];
+	int enc;
+	int indexPow2 = 0;
+
+	for(int i=0;i<message.length();i++){
+		enc = 1;
+		PowM[0] = 1;
+		PowM[1] = message[i];
+		indexPow2 = 0;
+
+		for(int j = 2; j<31; j++){
+			PowM[j] = (PowM[j-1]*PowM[j-1])%n;
+		}
+
+		while(true){
+			if(Pow2[indexPow2] > e){
+				break;
+			}
+			if(e&Pow2[indexPow2] > 0){
+				enc = enc * PowM[indexPow2];
+				if(enc > n){
+					enc = enc %n;
+				}
+			}
+			indexPow2++;
+		}
+		result[i] = enc;
+
+	}
+}
+
+
