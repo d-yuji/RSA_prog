3f8c5eed126cabd7e8cda4acf617b3a884bd9c22
YujiDouken <d.yuji.fm@gmail.com>
2017-06-08 01:27:48 +0900

    add comments

--- RSAprog.cpp
+++ RSAprog.cpp
@@ -8,23 +8,24 @@
 
 using namespace std;
 
-int gcd(int p,int q);//最大公約数
-int lcm(int p,int q);//最小公倍数
-void swap(int *a,int *b);//入れ替え
-int makeE(int l);//eの作成
-int makeD(int l,int e);//dの作成
-void encryption(string message,int mlength,int e,int n,int *encResult);//暗号化
-void decryption(int *encResult,int mlength,int d,int n,int *decResult);//復号化
+int gcd(int p,int q); //最大公約数
+int lcm(int p,int q); //最小公倍数
+void swap(int *a,int *b); //入れ替え
+int makeE(int l); //eの作成
+int makeD(int l,int e); //dの作成
+void encryption(string message,int mlength,int e,int n,int *encResult); //暗号化
+void decryption(int *encResult,int mlength,int d,int n,int *decResult); //復号化
 
-int Pow2[POWNUM];
+int Pow2[POWNUM]; //2の冪乗数の配列
 
 int main(int argc, char const *argv[]){
 
-	string message("Hello World!!");
-	int encResult[MAXMESSAGE];
-	int decResult[MAXMESSAGE];
-	int mlength = message.length();
+	string message("Hello World!!"); //暗号化される平文
+	int encResult[MAXMESSAGE]; //暗号結果の配列
+	int decResult[MAXMESSAGE]; //復号結果の配列
+	int mlength = message.length(); //平文の長さ
 
+	//値の初期化
 	int p = 227;
 	int q = 4099;
 	int n = p*q;
@@ -34,15 +35,16 @@ int main(int argc, char const *argv[]){
 		Pow2[i] = Pow2[i-1]*2;
 	}
 
+	//値の生成
 	int lambda = lcm(p-1,q-1);
 	int e = makeE(lambda);
 	int d = makeD(lambda,e);
 
 	printf("plain text\n");
 	printf("%s\n",message.c_str());
-	
 	printf("n:%d lambda:%d e:%d d:%d\n",n,lambda,e,d);
-	
+
+	//暗号化
 	encryption(message,mlength,e,n,encResult);
 
 	printf("encryption results::\n");
@@ -51,6 +53,7 @@ int main(int argc, char const *argv[]){
 	}
 	printf("\n");
 
+	//復号化
 	decryption(encResult,message.length(),d,n,decResult);
 
 	printf("decryption results\n");
@@ -60,6 +63,13 @@ int main(int argc, char const *argv[]){
 	printf("\n");
 	
 	return 0;
+}
+void swap(int *a,int *b){
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+	return;
 }
 
 int gcd(int p,int q){
@@ -80,13 +90,6 @@ int gcd(int p,int q){
 
 int lcm(int p,int q){
 	return p*q/gcd(p,q);
-}
-void swap(int *a,int *b){
-	int temp;
-	temp = *a;
-	*a = *b;
-	*b = temp;
-	return;
 }
 
 int makeE(int l){
@@ -110,20 +113,24 @@ int makeD(int l,int e){
 	return d;
 }
 void encryption(string message,int mlength,int e,int n,int* encResult){
-	long int PowM[POWNUM];// m^2k mod n (k > 1) の配列
-	long int enc;//暗号化された値
-	int indexPow2 = 0;//2の冪乗のインデックス
+	long int PowM[POWNUM]; //m^2k mod n (k > 1) の配列
+	long int enc; //暗号化された値
+	int indexPow2 = 0; //2の冪乗のインデックス
 
 	for(int i = 0;i < mlength;i++){
+		//値の初期化
 		enc = 1;
-		PowM[0] = message[i] % n;
 		indexPow2 = 0;
 
+		//暗号化する値の冪乗におけるmodを予め求める
+		PowM[0] = message[i] % n;
 		for(int j = 1; j < POWNUM-1; j++){
 			PowM[j] = (PowM[j-1] * PowM[j-1]) % n;
 		}
 
 		while(Pow2[indexPow2] <= e){
+
+			//ビット演算&で0以上、2進数で表したときに1となる桁の冪乗を掛けて計算
 			if((e&Pow2[indexPow2]) > 0){
 				enc = enc * PowM[indexPow2];
 				enc = enc %n;
@@ -137,20 +144,24 @@ void encryption(string message,int mlength,int e,int n,int* encResult){
 }
 
 void decryption(int *encResult,int mlength,int d,int n,int* decResult){
-	long int PowD[POWNUM];
-	long int dec;
-	int indexPow2 = 0;
+	long int PowD[POWNUM]; // d^2k mod n (k > 1) の配列
+	long int dec; //復号化された値
+	int indexPow2 = 0; //2の冪乗のインデックス
 
 	for(int i = 0; i < mlength; i++){
+		//値の初期化
 		dec = 1;
-		PowD[0] = encResult[i] % n;
 		indexPow2 = 0;
 
+		//復号化する値の冪乗におけるmodを予め求める
+		PowD[0] = encResult[i] % n;
 		for(int j = 1; j < POWNUM-1; j++){
 			PowD[j] = (PowD[j-1] * PowD[j-1]) % n;
 		}
 
 		while(Pow2[indexPow2] <= d){
+			
+			//ビット演算&で0以上、2進数で表したときに1となる桁の冪乗を掛けて計算
 			if((d&Pow2[indexPow2]) > 0){
 				dec = dec * PowD[indexPow2];
 				dec = dec % n;
